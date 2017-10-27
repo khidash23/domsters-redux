ff658f9ee58d46188ec474f2877121c51153f78d
Phil Howley <howley@IBDPRIL7ZJ1WL.local>
2017-10-27 14:37:14 -0700

    almost finish sprite 2 notes

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,5 +1,6 @@
 var gulp = require("gulp"),
-  svgSprite = require("gulp-svg-sprite");
+  svgSprite = require("gulp-svg-sprite"),
+  rename = require("gulp-rename");
 
 var config = {
   mode: {
@@ -14,8 +15,23 @@ var config = {
 };
 
 gulp.task("createSprite", function() {
-  return gulp
-    .src("./app/assets/images/icons/**/*.svg")
-    .pipe(svgSprite(config))
-    .pipe(gulp.dest("./app/temp/sprite/"));
+  return (gulp
+      // grab all the svgs inside the icons folder
+      .src("./app/assets/images/icons/**/*.svg")
+      // use the template var
+      .pipe(svgSprite(config))
+      // output generated sprite to time file
+      .pipe(gulp.dest("./app/temp/sprite/")) );
 });
+
+gulp.task("copySpriteCSS", ["createSprite"], function() {
+  return (gulp
+      // grab all the sprites generated
+      .src("./app/temp/sprite/css/*.css")
+      // rename to partial `_sprites`
+      .pipe(rename("_sprite.css"))
+      // copy file and move copy to modules directory
+      .pipe(gulp.dest("./app/assets/styles/modules")) );
+});
+
+gulp.task("icons", ["createSprite", "copySpriteCSS"]);
