799b1fb1aafdeb02c618ee7f6717de1d15c8df73
PEH2 <howley.phil@gmail.com>
2017-10-28 19:36:46 -0700

    finish header part 1 notes

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,17 +1,19 @@
-var gulp = require('gulp'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import'),
-mixins = require('postcss-mixins');
+const postcss = require('gulp-postcss');
+const gulp = require('gulp');
+const autoprefixer = require('autoprefixer');
+const cssvars = require('postcss-simple-vars');
+const nested = require('postcss-nested');
+const cssImport = require('postcss-import');
+const hexrgba = require('postcss-hexrgba');
+const mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
-  return gulp.src('./app/assets/styles/styles.css')
-      .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
-      .on('error', function(error) {
-        console.log(error.toString());
-        this.emit('end');
-      })
-      .pipe(gulp.dest('./app/temp/styles'));
+  return gulp
+    .src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
+    .on('error', function(error) {
+      console.log(error.toString());
+      this.emit('end');
+    })
+    .pipe(gulp.dest('./app/temp/styles'));
 });
