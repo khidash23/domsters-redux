db163cfeb8b5601ad60585e67405872669a7a369
Phil Howley <howley@IBDPRIL7ZJ1WL.local>
2017-10-30 13:43:31 -0700

    complete sticky header

--- app/assets/js/App.js
+++ app/assets/js/App.js
@@ -1,5 +1,6 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 import $ from 'jquery';
 
 const mobileMenu = new MobileMenu();
@@ -7,3 +8,5 @@ const mobileMenu = new MobileMenu();
 // const revealOnScroll = new RevealOnScroll();
 new RevealOnScroll($('.feature-item'), '85%');
 new RevealOnScroll($('.technologies'), '60%');
+
+const stickyHeader = new StickyHeader();
