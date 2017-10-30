65e1af130c28c6f705316e4f4d87662283f394af
Phil Howley <howley@IBDPRIL7ZJ1WL.local>
2017-10-30 07:30:17 -0700

    complete jquery speghetti notes

--- app/assets/js/App.js
+++ app/assets/js/App.js
@@ -1,20 +1,3 @@
-var $ = require('jquery');
-// var Person = require('./modules/Person');
-import Person from './modules/Person';
+import MobileMenu from "./modules/MobileMenu";
 
-class Adult extends Person {
-  payTaxes() {
-    console.log(this.name + ' now owes $0 in taxes');
-  }
-}
-
-console.log('Webpack Automation test#3');
-
-var john = new Adult('John Doe', 'red');
-john.greet();
-john.payTaxes();
-
-var jane = new Person('Jane Doe', 'purple');
-jane.greet();
-
-$('h2').remove();
+const mobileMenu = new MobileMenu();
