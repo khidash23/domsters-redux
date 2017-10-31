cf4b9267f71264e6e6065c95edecbdc5b54c2acd
Phil Howley <howley@IBDPRIL7ZJ1WL.local>
2017-10-31 11:34:23 -0700

    final modal part 2 notes

--- app/assets/js/App.js
+++ app/assets/js/App.js
@@ -3,6 +3,7 @@ import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 import HighlightSection from './modules/HighlightSection';
 import SmoothScrolling from './modules/SmoothScrolling';
+import Modal from './modules/Modal';
 import $ from 'jquery';
 
 const mobileMenu = new MobileMenu();
@@ -14,3 +15,4 @@ new RevealOnScroll($('.technologies'), '60%');
 const stickyHeader = new StickyHeader();
 const highlightSection = new HighlightSection();
 const smoothScrolling = new SmoothScrolling();
+const modal = new Modal();
